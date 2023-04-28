1813852a3bcebca677d35b38c3ada7bb957090b6
Michel van Elswijk <michel@corelifeanalytics.com>
2023-04-28 14:45:06 +0200

    update challenge 2 code

--- motherforker-9000/challenge_2.py
+++ motherforker-9000/challenge_2.py
@@ -1,8 +1,13 @@
-import controller
+from controller import ZBController
 from helper import *
 import time
+import numpy as np
+import cv2
 
-class challenge2:
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+class Challenge2:
 	"""
 	1. An unknown object, typically of size ~ 7cm x 7cm x 7cm, and always clearly visible, is placed at a
 	certain distance d ϵ [100 cm, 150 cm] from Position A.
@@ -12,32 +17,53 @@ class challenge2:
 	"""
 
 	def __init__(self):
+		# META variables
+		known_object_width = 7  # The width of the object in centimeters.
+		known_object_width_measured_distance = 110  # The distance at which the object was measured.
+		observed_width = 15  # The width of the object in pixels.
+
 		# Setup ZB controller
-		self.ZBC = controller.ZBController()
-		curr_frame = openImage("images/sitterzaal/1679116970.84_90cm.jpg")
-		self.distance = self.getObjectDistance(curr_frame)
+		self.ZBC = ZBController(False)
+		# Setup Camera & ImageProcessor
+		self.camera = Camera()
+		self.camera.intialize_focal_length(observed_width, known_object_width, known_object_width_measured_distance)
+
+		focal_length = self.camera.get_focal_length()
+		print("Initialized focal length: %d" % focal_length)
+
+		# curr_frame = self.take_curr_frame()
+
+		self.distance = 100#self.getObjectDistance(curr_frame)
 		print('Initialisation done, object is %d CM away' % self.distance)
 
 	def avoid_object(self):
 		print('Todo')
 
-	def getObjectDistance(self, curr_frame):
-		img_edge = cropImageTop(cannyEdgeDet(curr_frame))
-		return determineDistance(img_edge)
+	# def getObjectDistance(self, curr_frame):
+	# 	# img_edge = cropImageTop(cannyEdgeDet(curr_frame))
+	# 	img_edge = self.imager.
+	# 	return determineDistance(img_edge)
+
+	def take_curr_frame(self):
+		self.camera.take_picture('curr_frame.jpg')
 
 	def run(self):
 		print('We are on the way!')
-		self.ZBC.active_commands["forward"] = self.ZBC.current_time + 100
-		done = True
+		# self.ZBC.active_commands["forward"] = self.ZBC.current_time + 100
+		done = False
 		while not done:
-			self.ZBC.update_active_commands()
-			self.ZBC.update_servos()
-			time.sleep(0.1)
+			# self.ZBC.update_active_commands()
+			# self.ZBC.update_servos()
+			# time.sleep(0.1)
 
-			if _:
-				done = False
+			# Take image
+			self.take_curr_frame()
+			# print(img)
+			print('test')
+			done = True
+
 
 if __name__ == '__main__':
-	challenge = challenge2()
+	challenge = Challenge2()
 	challenge.run()
 	print('Done')
