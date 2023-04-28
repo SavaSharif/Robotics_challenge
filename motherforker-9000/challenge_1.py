9388139131285a8c2a5ff28d9d4645b9493cc764
Sava Sharif <savasharif@gmail.com>
2023-04-28 12:43:26 +0200

    use new controller

--- motherforker-9000/challenge_1.py
+++ motherforker-9000/challenge_1.py
@@ -1,11 +1,6 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 
-from motherforker_controller import ZBController
+from controller import ZBController
 from helper import Camera, ImageProcessor
-import cv2
-import numpy as np
-import os
 
 
 def main():
@@ -14,6 +9,7 @@ def main():
     known_object_width = 7 # The width of the object in centimeters.
     known_object_width_measured_distance = 110 # The distance at which the object was measured.
     observed_width = 15 # The width of the object in pixels.
+    robot = ZBController()
 
     camera = Camera()
     camera.intialize_focal_length(observed_width, known_object_width, known_object_width_measured_distance)
@@ -22,8 +18,8 @@ def main():
 
     reached_object = False
     while not reached_object:
-        # filename = camera.take_picture()
-        filename = os.path.join(os.getcwd(), "motherforker-9000/images/sitterzaal/90cm.jpg")
+        filename = camera.take_picture()
+        # filename = os.path.join(os.getcwd(), "motherforker-9000/images/sitterzaal/90cm.jpg")
         print("filename:", filename)
         img_processor = ImageProcessor(filename)
         observed_width = img_processor.get_object_width() # The width of the object in pixels.
@@ -34,6 +30,7 @@ def main():
         # Calculate the distance to the object.
         distance = (known_object_width * focal_length) / observed_width
         print("distance:", distance)
+        robot.move(direction="forward",distdeg = distance)
 
 
 if __name__ == '__main__':
