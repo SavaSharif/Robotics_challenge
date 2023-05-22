8dede7136ee880a69c77ac29cb0f482ce7e1131c
vikkyb <victorbatenburg@hotmail.com>
2023-05-22 11:21:38 +0200

    created demo file

---
+++ motherforker-9000/demo_22_5_23.py
@@ -0,0 +1,18 @@
+from controller import ZBController
+from helper import Camera, ImageProcessor, Forker
+
+def main():
+    """
+    Minimaal:
+    - Oppakken
+    - Neerzetten
+
+    Het liefst ook:
+    - Rij naar object
+    - Verplaats object
+    """
+
+    pass
+
+if __name__ == '__main__':
+    main()
