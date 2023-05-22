6e96c8484e5f970b0368379da03d653c7c12ffd8
Sava Sharif <savasharif@gmail.com>
2023-05-22 11:41:50 +0200

    try challenge 1 with pickup[

--- motherforker-9000/demo_22_5_23.py
+++ motherforker-9000/demo_22_5_23.py
@@ -1,5 +1,7 @@
+
 from controller import ZBController
 from helper import Camera, ImageProcessor, Forker
+
 
 def main():
     """
@@ -11,6 +13,25 @@ def main():
     - Rij naar object
     - Verplaats object
     """
+    robot = ZBController()
+
+    camera = Camera()
+    filename = camera.take_picture()
+    print("filename:", filename)
+    img_processor = ImageProcessor(filename)
+
+    lowest = img_processor.get_lowest_pixel()
+    distance = img_processor.get_distance(lowest)
+
+    print('Initialisation done, we are driving %d CM towards the object' % distance)
+    _ = input('Do you want to drive this distance?')
+    print('We are on the way!')
+
+    robot.move_once(direction="forward", distdeg = distance / 100)
+
+    forker = Forker()
+    forker.pickup_object()
+    forker.pulse_width_module_cleanup()
 
 
 if __name__ == '__main__':
